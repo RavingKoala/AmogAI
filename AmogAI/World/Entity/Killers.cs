b3f5825c0bd8ac23901f8b0e7e2b54360a4cfedd
Stijnvb <stijnvb@hotmail.com>
2023-12-13 16:28:18 +0100

    killer class

---
+++ AmogAI/World/Entity/Killers.cs
@@ -0,0 +1,13 @@
+﻿namespace AmogAI.World.Entity;
+
+using AmogAI.SteeringBehaviour;
+
+public class Killers : MovingEntity {
+    public Survivor? target;
+
+    Killers(Vector pos, World world) : base(pos, world) {
+        target = null;
+    }
+
+
+}
