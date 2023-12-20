73cd6769a7872d573d2431265dd7a328497c081d
Stijnvb <stijnvb@hotmail.com>
2023-12-20 12:16:39 +0100

    removed killer cone code, removed redundant entity to survivor code

--- AmogAI/StateBehaviour/KillerStates/WanderState.cs
+++ AmogAI/StateBehaviour/KillerStates/WanderState.cs
@@ -10,22 +10,13 @@ public class WanderState : IState<Killer> {
     }
 
     public void Execute(Killer killer, float timeDelta) {
-        foreach (MovingEntity entity in killer.World.Survivors) {
-            if (entity.GetType() != typeof(Survivor))
-                continue;
-            Survivor survivor = (Survivor)entity;
+        foreach (Survivor survivor in killer.World.Survivors) {
 
             // Close proximity detection logic
             if (killer.Position.Distance(survivor.Position) < killer.DetectionRadius) {
                 killer.Target = survivor;
                 killer.StateMachine.ChangeState(new KillState());
             }
-
-            // Cone detection logic
-            (Vector point1, Vector point2) = killer.CalculateDetectionCone();
-            // if any enemies in triangle (p1-p2-Position)
-                // if != a wall inbetween
-                    // set as target
         }
     }
 
